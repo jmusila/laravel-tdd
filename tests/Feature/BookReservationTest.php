7af2e86aa73a68fa952a1c8b794046a7e2aee236
jonathan <jonathan.muthiani@squaddigital.com>
2020-05-04 11:14:45 +0300

    added book test method

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -10,6 +10,12 @@ class BookReservationTest
 {
     /** @test */
     public function test_a_book_can_be_added_to_the_libraly(){
-        
+        $this->post('/books', [
+            'title' => 'Cool book title',
+            'author' => 'John Doe',
+        ]);
+
+        $this->assertOk();
+        $this->assertCount(1, Book::all());
     }
 }
